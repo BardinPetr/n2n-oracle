df2cbdf8ded04a0a213d06c5aba89887ae22d46b
Bardin Petr <bardin.petr@gmail.com>
2021-03-22 10:31:33 +0300

    Basic deploy

---
+++ src/deploy.py
@@ -0,0 +1,64 @@
+import os
+
+import dotenv
+import solcx
+from web3 import Web3, HTTPProvider
+
+from src.utils.tools import to_address
+
+dotenv.load_dotenv(verbose=True, override=True)
+
+PRIVKEY = os.getenv("PRIVKEY")
+
+SOLCV = os.getenv("SOLC_VERSION", "0.7.6")
+SOLCP = os.getenv("SOLC_PATH", os.getcwd() + "/solcx")
+try:
+    os.makedirs(SOLCP)
+except:
+    pass
+
+cnt = 0
+
+
+def log(net, data):
+    global cnt
+    cnt += 1
+    print(f"#{cnt} [{net}]", data)
+
+
+def deploy(contract, name, network="LEFT"):
+    web3 = Web3(HTTPProvider(os.getenv(network + "_RPCURL")))
+
+    tx = web3.eth \
+        .contract(bytecode=contract["bin"], abi=contract["abi"]) \
+        .constructor(os.getenv("VALIDATORS"), os.getenv("THRESHOLD")) \
+        .buildTransaction({'gasPrice': int(os.getenv(network + "_GASPRICE")),
+                           'nonce': web3.eth.getTransactionCount(to_address(PRIVKEY))})
+
+    signed = web3.eth.account.signTransaction(tx, private_key=PRIVKEY)
+
+    tx_hash = web3.eth.sendRawTransaction(signed.rawTransaction)
+    txr = web3.eth.waitForTransactionReceipt(tx_hash)
+
+    log(network, f"{name} deployed at {txr['contractAddress']}")
+    if name == 'Bridge':
+        log(network, f"{name} deployed at block {txr['blockHash'].hex()}")
+    return txr
+
+
+def main():
+    solcx.install_solc(SOLCV, solcx_binary_path=SOLCP)
+    solcx.set_solc_version(SOLCV, solcx_binary_path=SOLCP)
+    base_dir = "src/contracts/"
+    contracts = [("Bridge", "Bridge.sol"), ("Validators Set", "ValidatorSet.sol")]
+    res = solcx.compile_files([base_dir + i[1] for i in contracts],
+                              optimize=True,
+                              optimize_runs=200)
+
+    for net in ["LEFT", "RIGHT"]:
+        for name, file in contracts:
+            deploy(res.get(f"{base_dir}{file}:{file.removesuffix('.sol')}"), name, net)
+
+
+if __name__ == '__main__':
+    main()
