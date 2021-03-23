21f99e1192593e7d4d83244ee828ff0936438a4d
Bardin Petr <bardin.petr@gmail.com>
2021-03-23 19:43:50 +0300

    Trying to fix [skip ci]

--- src/oracle.py
+++ src/oracle.py
@@ -6,9 +6,10 @@ import sys
 import time
 
 import dotenv
-from web3 import Web3, HTTPProvider
+from web3 import Web3, HTTPProvider, middleware
 from web3.exceptions import ContractLogicError
 from web3.middleware import geth_poa_middleware
+from web3.gas_strategies.time_based import medium_gas_price_strategy, construct_time_based_gas_price_strategy
 
 from utils.contract_wrapper import ContractWrapper
 from utils.tools import install_solc, to_address, get_ABI
@@ -16,19 +17,25 @@ from utils.tools import install_solc, to_address, get_ABI
 dotenv.load_dotenv(verbose=True, override=True)
 logging.basicConfig(level=logging.INFO)
 
-mount_point = ("/data" if "deployment" in os.getcwd() else os.getenv("ORACLE_DATA")).removesuffix("/")
+in_docker = "/deployment" in os.getcwd()
+mount_point = ("/data" if in_docker else os.getenv("ORACLE_DATA")).removesuffix("/")
 logfile = f"{mount_point}/{int(time.time())}.log"
 
 
 def log(*args, console=True):
     if console:
         logging.info(*args)
-    with open(logfile, "a") as f:
-        print(f"[{time.ctime()}]", *args, file=f)
+    try:
+        with open(logfile, "a") as f:
+            print(f"[{time.ctime()}]", *args, file=f)
+    except FileNotFoundError:
+        pass
 
 
+log("\n\n" + "#" * 15 + "\n\n")
 log("Started")
-install_solc()
+if in_docker:
+    install_solc()
 log("Solc prepared")
 
 # # load and store database
@@ -42,9 +49,11 @@ except:
 
 def exit_gracefully(*args):
     log(f"Saving {len(processed.keys())} items")
-    with open(f"{mount_point}/db.json", "w") as f:
-        json.dump(processed, f)
-    sys.exit(0)
+    try:
+        with open(f"{mount_point}/db.json", "w") as f:
+            json.dump(processed, f)
+    finally:
+        sys.exit(0)
 
 
 for i in [signal.SIGINT, signal.SIGTERM]:
@@ -62,17 +71,28 @@ sb = (int(os.getenv("LEFT_START_BLOCK")), int(os.getenv("RIGHT_START_BLOCK")))
 def create(x):
     w = Web3(HTTPProvider(os.getenv(x + "_RPCURL")))
     w.middleware_onion.inject(geth_poa_middleware, layer=0)
+
+    w.eth.defaultAccount = w.eth.account.from_key(priv_key)
+    w.eth.custom_defaultGasPrice = gas[0 if x == "LEFT" else 1]
     return w
 
 
 web3 = [create(i) for i in ["LEFT", "RIGHT"]]
 # web3 = (Web3(HTTPProvider(os.getenv("LEFT_RPCURL"))), Web3(HTTPProvider(os.getenv("RIGHT_RPCURL"))))
 
+
 # Contracts
-abi = get_ABI("src/contracts/", "BridgeSide")
+abi_path = "src/contracts/abi.json"
+abi = None
+try:
+    with open(abi_path, "r") as f:
+        abi = json.load(f)
+except:
+    abi = get_ABI("src/contracts/", "BridgeSide")
+    json.dump(abi, open(abi_path, "w"))
 
 contract = [
-    ContractWrapper(w3=web3[i], gas=gas[i], user_pk=priv_key, abi=abi, address=contract_addr[i])
+    ContractWrapper(w3=web3[i], gas_price=gas[i], user_pk=priv_key, abi=abi, address=contract_addr[i])
     for i in range(len(contract_addr))
 ]
 
@@ -95,7 +115,8 @@ def update(flt, startup=False):
                     latest_event_where_im_not_a_validator[i] = None
             return False
 
-        logs = flt[i].get_all_entries() # if startup else flt[i].get_new_entries()
+        logs = flt[i].get_all_entries() if startup else flt[i].get_new_entries()
+
         for e in logs:
             data = (e['args']['recipient'], e['args']['amount'], int.from_bytes(e['transactionHash'], 'big'))
             tid = Web3.solidityKeccak(['address', 'uint256', 'uint32'], data)
@@ -115,7 +136,8 @@ def update(flt, startup=False):
                     log(f"Failed for {xid}", ex)
                     # TODO: maybe save failed commit
             else:
-                log(f"OLD event on NET{i} from {data[0]} with amount {data[1]} with ID{xid}")
+                pass
+                # log(f"OLD event on NET{i} from {data[0]} with amount {data[1]} with ID{xid}")
     return found_any
 
 
@@ -123,6 +145,15 @@ def main():
     log(f"Working from {acc}")
 
     flt = [contract[i].events.bridgeActionInitiated.createFilter(fromBlock=sb[i]) for i in range(2)]
+    # flt = [
+    #     web3[i].eth.filter({
+    #         "address": contract_addr[0],
+    #         "fromBlock": sb[i],
+    #         "toBlock": "latest",
+    #         "topics": ["0x4c110dd0476ad6b36205086604df9912b3ca7ffb917ec4e6a41403474b6cd937"]
+    #     })
+    #     for i in range(2)
+    # ]
 
     log("Running first update")
     if not update(flt, startup=True):
@@ -131,7 +162,9 @@ def main():
     log("Updating old events completed. Running continuously")
     try:
         while True:
+            # a = time.time()
             update(flt)
+            # print(time.time() - a)
     except KeyboardInterrupt:
         exit_gracefully()
 
--- src/utils/contract_wrapper.py
+++ src/utils/contract_wrapper.py
@@ -2,11 +2,7 @@ from .tools import to_address
 
 
 class ContractWrapper:
-    gas_price = None
-    user_priv_key = None
-    user_acc = None
-
-    def __init__(self, w3, gas, user_pk, **kwargs):
+    def __init__(self, w3, gas_price, user_pk, **kwargs):
         """
         Методы автоматически определяются как call или buildTransaction
         > методы определенные как call возвращают результат функции
@@ -16,11 +12,7 @@ class ContractWrapper:
             w3 (Web3): экземпляр web3 для общения с блокчеином.
             **kwargs (type): парамтры как для eth.contract().
         """
-
-        self.gas_price = gas
-        if user_pk is not None:
-            self.user_priv_key = user_pk
-            self.user_acc = to_address(user_pk)
+        user_acc = to_address(user_pk)
 
         contract = w3.eth.contract(**kwargs)
 
@@ -30,11 +22,11 @@ class ContractWrapper:
         # setup constructor
         def construct(*args, **kwargs):
             tx = contract.constructor(*args, **kwargs).buildTransaction({
-                'gasPrice': self.gas_price,
-                'nonce': w3.eth.getTransactionCount(self.user_acc)
+                'gasPrice': gas_price,
+                'nonce': w3.eth.getTransactionCount(user_acc)
             })
 
-            signed = w3.eth.account.signTransaction(tx, private_key=self.user_priv_key)
+            signed = w3.eth.account.signTransaction(tx, private_key=user_pk)
             tx_hash = w3.eth.sendRawTransaction(signed.rawTransaction)
 
             tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
@@ -59,11 +51,11 @@ class ContractWrapper:
                                 value = 0 if 'value' not in kwargs.keys() else kwargs.pop('value')
 
                                 tx = {
-                                    'from': self.user_acc,
+                                    'from': user_acc,
                                     'to': contract.address,
                                     'value': value,
-                                    'gasPrice': self.gas_price,
-                                    'nonce': w3.eth.getTransactionCount(self.user_acc),
+                                    'gasPrice': gas_price,
+                                    'nonce': w3.eth.getTransactionCount(user_acc),
                                     'data': contract.encodeABI(fn_name=name, args=args, kwargs=kwargs)
                                 }
 
@@ -79,7 +71,7 @@ class ContractWrapper:
 
                                 tx['gas'] = w3.eth.estimateGas(tx)
 
-                                signed = w3.eth.account.signTransaction(tx, private_key=self.user_priv_key)
+                                signed = w3.eth.account.signTransaction(tx, private_key=user_pk)
                                 tx_hash = w3.eth.sendRawTransaction(signed.rawTransaction)
                                 tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
 
